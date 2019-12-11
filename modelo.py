243cb6be252880a7246326077a1cbbfbb6b495db
Vitor <vitor.condexd@gmail.com>
2019-12-11 16:17:31 -0300

    Aula 02
    Herança, e remoção dos .py que não vamos mais utilizar.

--- modelo.py
+++ modelo.py
@@ -1,59 +1,47 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def set_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def set_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
-
-class Serie:
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def set_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
-
-
-vingadores = Filme("Vingadores - Guerra infinita", 2018, 160)
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
 vingadores.set_like()
 vingadores.set_like()
-print(f'Nome: {vingadores.nome}, Ano: {vingadores.ano}, Duração: {vingadores.duracao}, Likes: {vingadores.likes}')
+vingadores.set_like()
 
+atlanta.set_like()
+atlanta.set_like()
 
-atlanta = Serie("Atlanta", 2018, 2)
-print(f'Nome: {atlanta.nome}, Ano: {atlanta.ano}, Temporadas: {atlanta.temporadas}, Likes: {atlanta.likes}')
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
